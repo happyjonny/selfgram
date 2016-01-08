128973cca320895e9bc7ab71c6809c79ba037f41
happyjonny <jonnywithyqy@gmail.com>
2016-01-07 19:01:36 -0800

    Initial Commit

---
+++ selfgram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  selfgram
+//
+//  Created by LH on 2016-01-07.
+//  Copyright © 2016 LH-jonny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
